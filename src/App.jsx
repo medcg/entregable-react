964e0e50106b79f77f9e07504627721cfc544948
medcg <martindcg88@gmail.com>
2023-09-27 11:06:18 -0300

    correción

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import { useState } from "react";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const {search, setSearch} = useState("");
 
-export default App
+  const [user, setUser] = useState(null)
+
+  const fetchUser = async () => 
+  try { 
+const response = await fetch (`https://api.github.com/search/users?q=${nombre}`)
+const data = await response.json ();
+setUser (data);
+  } 
+  catch (error) { 
+    console.log("error", error.message);
+
+  }
+
+  return (
+    <div>
+      <h1>GitHub Searcher</h1>
+      <div>
+        <imput onChange={ (e) => setSearch(e.target.value)} placeholder="Ingresa Nombre de Usuario"></imput>
+      </div>
+      <div>
+        <button onClick={fetchUser}>Buscar</button>{" "}
+      </div>
+      <article>
+        {user && (
+          <div className="container"/>
+        <div img src={user.avatar.url} alt="avatar"/>
+        <h4>{user.login}</h4>
+        <p>{user.bio}</p>
+        )}
+        
+      </article>
+    </div>
+  );
+};
+
+export default App;
