5e1613965a44a01a7c08453dea386be86bb97d1b
medcg <martindcg88@gmail.com>
2023-10-12 15:51:43 -0300

    finalizado

--- src/components/pages/PaginaPrincipal.jsx
+++ src/components/pages/PaginaPrincipal.jsx
@@ -6,6 +6,7 @@ function PaginaPrincipal() {
     <div>
       <h1 className="text-3xl text-center text-blue-900 mt-4 mb-4 ">Buscador de GitHub</h1>
       <p className=" mt-4 mb-4 text-center">En este buscador puedes localizar tanto los usuarios como los repositorios de la manera más rápida y eficaz.</p>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/c/c2/GitHub_Invertocat_Logo.svg" alt="GitHub Invertocat Logo.svg" className="w-auto mx-auto" /> 
     </div>
   );
 }
